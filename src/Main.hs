c28e942a9501cef64a1d4ae6097b038775257347
Nikita Talalai <nikita.talalai.2002@gmail.com>
2024-04-30 21:58:35 +0300

    ci: fix ci errors

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.WindowSwallowing
 
---Layouts
+-- Layouts
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Tabbed
 
@@ -19,31 +19,30 @@ import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig
 import XMonad.Util.NamedScratchpad
 
+import Data.Map qualified as M
+import XMonad.StackSet qualified as W
 
-import qualified Data.Map        as M
-import qualified XMonad.StackSet as W
-
-myTerminal      = "alacritty"
+myTerminal = "alacritty"
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
 
-myBorderWidth   = 1
+myBorderWidth = 1
 
-myModMask       = mod4Mask
+myModMask = mod4Mask
 
-myNormalBorderColor  = "#dddddd"
+myNormalBorderColor = "#dddddd"
 myFocusedBorderColor = "#ff0000"
 
 myLayout = avoidStrutsOn [U] (noBorders Full ||| tiled ||| Mirror tiled ||| noBorders simpleTabbed)
   where
-    tiled   = Tall nmaster delta ratio
+    tiled = Tall nmaster delta ratio
     nmaster = 1
-    ratio   = 1/2
-    delta   = 3/100
+    ratio = 1 / 2
+    delta = 3 / 100
 
-scratchpads = [ NS "term" "alacritty -T scratchpad" queryTerm manageTerm]
-  where 
+scratchpads = [NS "term" "alacritty -T scratchpad" queryTerm manageTerm]
+  where
     queryTerm = title =? "scratchpad"
     manageTerm = customFloating $ W.RationalRect xOffset yOffset width height
       where
@@ -52,45 +51,46 @@ scratchpads = [ NS "term" "alacritty -T scratchpad" queryTerm manageTerm]
         xOffset = 0.05
         yOffset = 0.05
 
-myWorkspaces    = ["web","read","notes","chats","term","6","7","8","9"]
+myWorkspaces = ["web", "read", "notes", "chats", "term", "6", "7", "8", "9"]
 
-myManageHook = composeOne [ 
-    appName   =? "Places"           -?> doShift "web" <+> (doRectFloat $ W.RationalRect 0 0 0.5 0.5),
-    className =? "firefox"          -?> doShift "web",
-    className =? "Zathura"          -?> doShift "read",
-    className =? "obsidian"         -?> doShift "notes",
-    className =? "TelegramDesktop"  -?> doShift "chats",
-    className =? "Alacritty"        -?> doShift "term",
-    isFullscreen  -?> doFullFloat,
-    isDialog      -?> doRectFloat $ W.RationalRect 0 0 0.3 0.3
-    -- namedScratchpadManageHook scratchpads 
+myManageHook =
+  composeOne
+    [ appName =? "Places" -?> doShift "web" <+> doRectFloat (W.RationalRect 0 0 0.5 0.5)
+    , className =? "firefox" -?> doShift "web"
+    , className =? "Zathura" -?> doShift "read"
+    , className =? "obsidian" -?> doShift "notes"
+    , className =? "TelegramDesktop" -?> doShift "chats"
+    , className =? "Alacritty" -?> doShift "term"
+    , isFullscreen -?> doFullFloat
+    , isDialog -?> doRectFloat $ W.RationalRect 0 0 0.3 0.3
+    -- namedScratchpadManageHook scratchpads
     ]
 
 myStartupHook :: X ()
-myStartupHook = 
+myStartupHook =
   spawn "myxmobar"
 
 myHandleEventHook = swallowEventHook (className =? "Alacritty") (return True)
 
-main = xmonad $ docks . (withMetrics def) $ myConfig
+main = xmonad $ docks . withMetrics def $ myConfig
 
-myConfig = def
-  { terminal = myTerminal
-  , modMask = mod4Mask
-  , layoutHook = myLayout
-  , focusFollowsMouse = False
-  , borderWidth = 1
-  , workspaces = myWorkspaces
-  , normalBorderColor = "#dddddd"
-  , focusedBorderColor = "#ff0000"
-  , manageHook = myManageHook
-  , startupHook = myStartupHook
-  , handleEventHook = myHandleEventHook
-  }
-  `additionalKeysP`
-  [ ("M-f", spawn "firefox")
-  , ("M-r", spawn "rofi -show drun")
-  , ("M-o", namedScratchpadAction scratchpads "term")
-  , ("M-S-b", sendMessage ToggleStruts)
-  , ("M-S-x", restart "/run/current-system/sw/bin/myxmonad" True)
-  ]
+myConfig =
+  def
+    { terminal = myTerminal
+    , modMask = mod4Mask
+    , layoutHook = myLayout
+    , focusFollowsMouse = False
+    , borderWidth = 1
+    , workspaces = myWorkspaces
+    , normalBorderColor = "#dddddd"
+    , focusedBorderColor = "#ff0000"
+    , manageHook = myManageHook
+    , startupHook = myStartupHook
+    , handleEventHook = myHandleEventHook
+    }
+    `additionalKeysP` [ ("M-f", spawn "firefox")
+                      , ("M-r", spawn "rofi -show drun")
+                      , ("M-o", namedScratchpadAction scratchpads "term")
+                      , ("M-S-b", sendMessage ToggleStruts)
+                      , ("M-S-x", restart "/run/current-system/sw/bin/myxmonad" True)
+                      ]
